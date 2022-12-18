2936ad6d5ccb9645af3f6f5b166f51b911dd11e7
jay-nakagawa <j.dickson2203@gmail.com>
2022-12-17 21:07:07 -0500

    push

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import { useState, useEffect } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+
+console.log('test')
+
+
+
+
+function App() {
+  const [cats, setCats] = useState([])
+ useEffect(()=> { 
+  fetch("https://defiant-dungarees-jay.cyclic.app/cats", {
+    headers: {
+      'content-type': 'application/json'
+    }
+  }).then(response => response.json())
+  .then(data => {
+    setCats(data)
+  })
+ } )
+  return (
+    <div className="App">
+    <h1>cats</h1>
+    {cats.map(cat => (
+    <div key={cat._id}>
+       The cat is named {cat.name}
+      
+    </div>
+    ))}
+    </div>
+  )
+}
+
+export default App
